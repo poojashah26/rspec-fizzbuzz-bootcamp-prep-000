fb36ddb984c3e757eb298d982611a6c466bd9e08
Pooja Shah <pooja_shah26@hotmail.co.uk>
2019-09-30 16:29:05 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(int)
-  if int % 3 == 0
-    "Fizz"
-  elsif int % 5 == 0
-    "Buzz"
-  elsif int % 3 == 0 && 5 == 0
-    "FizzBuzz"
-  end
+def fizzbuzz(i)
+  if i % 5 == 0 and i % 3 == 0
+        puts "FizzBuzz"
+      elsif i % 5 == 0
+        puts "Buzz"
+      elsif i % 3 == 0
+        puts "Fizz"
+      else
+        puts i
+    end
 end
 
 
